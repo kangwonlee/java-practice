6a6f73216c657601ba41c38d8385ca78cf48fc41
KangWon LEE <kangwon@gmail.com>
2023-12-18 22:06:57 +0900

    first java code

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// https://stackoverflow.com/questions/2360995/how-do-i-compile-and-run-a-program-in-java-on-my-mac
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
+
