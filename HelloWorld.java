ab2bcc7f3c6dece60306449d95869804fee486c3
KangWon LEE <kangwon@gmail.com>
2023-12-19 04:45:49 +0900

    added comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,17 @@
 // https://stackoverflow.com/questions/2360995/how-do-i-compile-and-run-a-program-in-java-on-my-mac
+
+/**
+ * The HelloWorld class is a simple Java program that prints "Hello World!" to the console.
+ */
 public class HelloWorld {
-    public static void main(String args[]) {
+    /**
+     * The main method is the entry point of the program.
+     * It prints "Hello World!" to the console.
+     *
+     * @param args The command-line arguments passed to the program (not used in this program).
+     */
+
+     public static void main(String args[]) {
         System.out.println("Hello World!");
     }
 }
-
